e4e647697eada5a800a4cc294e58b9de22c27d38
Hung Yu-Ting <r96941046@gmail.com>
2016-11-21 17:39:29 +0800

    More code

--- chapter4-turtle.py
+++ chapter4-turtle.py
@@ -3,11 +3,39 @@ import sys
 from PySide import QtCore, QtGui
 
 
-import ipdb; ipdb.set_trace();
-
-class TurtleControl(QWidget):
+class TurtleControl(QtGui.QWidget):
 
     def __init__(self, turtle):
 
         super(TurtleControl, self).__init__()
         self.turtle = turtle
+
+        self.left_btn = QtGui.QPushButton('Left', self)
+        self.right_btn = QtGui.QPushButton('Right', self)
+        self.move_btn = QtGui.QPushButton('Move', self)
+
+        self.distance_spin = QtGui.QSpinBox()
+
+        self.controlLayout = QtGui.QGridLayout()
+        self.controlLayout.addWidget(self.left_btn, 0, 0)
+        self.controlLayout.addWidget(self.right_btn, 0, 1)
+        self.controlLayout.addWidget(self.distance_spin, 1, 0)
+        self.controlLayout.addWidget(self.move_btn, 1, 1)
+        self.setLayout(self.controlLayout)
+
+        self.distance_spin.setRange(0, 100)
+        self.distance_spin.setSingleStep(5)
+        self.distance_spin.setValue(20)
+
+# Setup turtle
+window = turtle.Screen()
+babbage = turtle.Turtle()
+
+# Create a Qt application
+app = QtGui.QApplication(sys.argv)
+control_window = TurtleControl(babbage)
+control_window.show()
+
+# Enter Qt application
+app.exec_()
+sys.exit()
